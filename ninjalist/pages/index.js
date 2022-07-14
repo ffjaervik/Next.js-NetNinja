24f6600882d6a84eedc0177c98ea7f96f4e2984c
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-07-14 18:59:25 +0100

    added the layout and wrapped it around the {children}

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -8,14 +8,13 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <Navbar/>
       <h1> Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quaerat inventore hic excepturi natus autem deserunt ab quia alias maxime, sunt saepe porro adipisci! Laborum necessitatibus nobis quibusdam itaque libero.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quaerat inventore hic excepturi natus autem deserunt ab quia alias maxime, sunt saepe porro adipisci! Laborum necessitatibus nobis quibusdam itaque libero.</p>
       <Link href="/ninjas">
        <a>See Ninja Listing</a>
       </Link>
-      <Footer/>
+
     </div>
   )
 }
