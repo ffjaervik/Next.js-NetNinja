2fdedce81a30e746002ac9a27683bfe57405fb37
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-07-14 20:02:50 +0100

    added css globals & components

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -8,11 +8,11 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <h1> Homepage</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quaerat inventore hic excepturi natus autem deserunt ab quia alias maxime, sunt saepe porro adipisci! Laborum necessitatibus nobis quibusdam itaque libero.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quaerat inventore hic excepturi natus autem deserunt ab quia alias maxime, sunt saepe porro adipisci! Laborum necessitatibus nobis quibusdam itaque libero.</p>
+      <h1 className={styles.title}> Homepage</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quaerat inventore hic excepturi natus autem deserunt ab quia alias maxime, sunt saepe porro adipisci! Laborum necessitatibus nobis quibusdam itaque libero.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quaerat inventore hic excepturi natus autem deserunt ab quia alias maxime, sunt saepe porro adipisci! Laborum necessitatibus nobis quibusdam itaque libero.</p>
       <Link href="/ninjas">
-       <a>See Ninja Listing</a>
+       <a className={styles.btn}>See Ninja Listing</a>
       </Link>
 
     </div>
