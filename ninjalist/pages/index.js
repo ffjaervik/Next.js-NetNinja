f469b107b76e583f6c33bf0824f04141f9538640
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-07-15 08:56:07 +0100

    added navbar image & metadata for Home & About page

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -7,14 +7,20 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}> Homepage</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quaerat inventore hic excepturi natus autem deserunt ab quia alias maxime, sunt saepe porro adipisci! Laborum necessitatibus nobis quibusdam itaque libero.</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quaerat inventore hic excepturi natus autem deserunt ab quia alias maxime, sunt saepe porro adipisci! Laborum necessitatibus nobis quibusdam itaque libero.</p>
-      <Link href="/ninjas">
-       <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content ="ninjas"/>
+      </Head>
+      <div>
+        <h1 className={styles.title}> Homepage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quaerat inventore hic excepturi natus autem deserunt ab quia alias maxime, sunt saepe porro adipisci! Laborum necessitatibus nobis quibusdam itaque libero.</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quaerat inventore hic excepturi natus autem deserunt ab quia alias maxime, sunt saepe porro adipisci! Laborum necessitatibus nobis quibusdam itaque libero.</p>
+        <Link href="/ninjas">
+        <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
 
-    </div>
+      </div>
+    </>
   )
 }
