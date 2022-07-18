b030f3a5b953bed97abb33967a1123d0c1db1ed0
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-07-18 12:05:01 +0100

    fetching and displaying the single item into its own url path

--- ninjalist/pages/ninjas/[id].js
+++ ninjalist/pages/ninjas/[id].js
@@ -1,6 +1,7 @@
 export const getStaticPaths = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
+    
 
     const paths = data.map(ninja => {
         return {
@@ -12,16 +13,28 @@ export const getStaticPaths = async () => {
         paths,
         fallback: false
     }
+}   
 
-    }   
+export const getStaticProps = async ( context ) => {
+   const id = context.params.id;
+   const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+   const data = await res.json();
+   
+
+    return {
+        props: { ninja: data }
+    }
+}
 
 
-const Details = () => {
+const Details = ({ninja}) => {
     return ( 
         <div>
-            <h1>Details Page</h1>
-
-
+            <h1>{ninja.name}</h1>
+            <p>{ninja.email}</p>
+            <p>{ninja.website}</p>
+            <p>{ninja.company.catchPhrase }</p>
+            <p>{ninja.address.city }</p>
         </div>
      );
 }
