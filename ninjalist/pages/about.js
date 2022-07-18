c8e73989415c8a59840eabb32bb9d44f7d44c3f2
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-07-18 12:37:09 +0100

    testing automatic deployment

--- ninjalist/pages/about.js
+++ ninjalist/pages/about.js
@@ -10,7 +10,7 @@ const About = () => {
                 <meta name="keywords" content ="ninjas"/>
             </Head>
             <div>
-                <h1>About</h1>
+                <h1>About Ninjas</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci deleniti iure laudantium eius modi distinctio dicta? Aliquam maxime hic alias. Quos architecto ipsum sequi accusantium quaerat perspiciatis animi ipsa doloremque?</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci deleniti iure laudantium eius modi distinctio dicta? Aliquam maxime hic alias. Quos architecto ipsum sequi accusantium quaerat perspiciatis animi ipsa doloremque?</p>
             </div>
